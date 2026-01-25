00d661e8e477222ad6eaa132ee6e620d5fedee29
ChandanaR <54231820+ChandanaRaghavendren@users.noreply.github.com>
2026-01-25 20:11:20 +0530

    Created .net c# basic singleton design pattern

---
+++ SingleTon/SingleTon/DBClass.cs
@@ -0,0 +1,66 @@
+﻿namespace SingleTon
+{
+    internal class DBClass
+    {
+        private static DBClass instance = null;
+
+        String dbUrl;
+        String userName;
+        String password;
+
+        private static object syncLock = new object();
+        public static DBClass getInstance()
+        {
+            if (instance == null)
+            {
+                Console.WriteLine("DB accessed when null.");
+                lock (syncLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new DBClass();
+                        Console.WriteLine("DB created.");
+                    }
+                }
+            }
+            Console.WriteLine("DB returned.");
+            return instance;
+        }
+
+        private DBClass()
+        {
+
+        }
+
+
+        public String getDbUrl()
+        {
+            return dbUrl;
+        }
+
+        public void setDbUrl(String dbUrl)
+        {
+            this.dbUrl = dbUrl;
+        }
+
+        public String getUserName()
+        {
+            return userName;
+        }
+
+        public void setUserName(String userName)
+        {
+            this.userName = userName;
+        }
+
+        public String getPassword()
+        {
+            return password;
+        }
+
+        public void setPassword(String password)
+        {
+            this.password = password;
+        }
+    }
+}
---
+++ SingleTon/SingleTon/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SingleTon;
+
+DBClass dbc1 = DBClass.getInstance();
+DBClass dbc2 = DBClass.getInstance();
+Console.WriteLine(dbc1);
+Console.WriteLine(dbc2);
